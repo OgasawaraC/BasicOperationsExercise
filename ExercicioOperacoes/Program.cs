81bc256bbe68d27e70efc8741335fed13e8ddb5c
OgasawaraC <caueoga@gmail.com>
2023-04-29 01:26:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioOperacoes/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExercicioOperacoes;
+
+Console.WriteLine("Olá, boas vindas ao Calculador!");
+Operacoes operacoes = new Operacoes();
+Console.WriteLine("O valor é igual a = "+operacoes.Soma.Calcular(10,10));
+Console.WriteLine("O valor é igual a = " +operacoes.Subtracao.Calcular(30, 15));
+Console.WriteLine("O valor é igual a = " + operacoes.Divisao.Calcular(657, 30));
+Console.WriteLine("O valor é igual a = " + operacoes.Multiplicacao.Calcular(10, 10));
+
+Console.ReadKey();
